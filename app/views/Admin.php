aa5a55164ae36ef92b04684c75a2e0ec80170827
Pablo Bolaños <104400318+Pab1o16@users.noreply.github.com>
2024-09-27 15:58:14 -0600

    Update Admin.php

--- app/views/Admin.php
+++ app/views/Admin.php
@@ -1,4 +1,7 @@
 <?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     // Incluir la clase de manejo de sesiones
     require_once __DIR__ . '/../Controllers/MySQLSessionHandler.php';
 
